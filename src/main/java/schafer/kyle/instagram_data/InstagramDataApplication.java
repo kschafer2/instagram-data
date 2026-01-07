b90a21fdc3034e68a057851b2bd5f08ba21c8b18
Kyle Schafer <kschay@protonmail.com>
2026-01-06 21:31:06 -0700

    Initial commit

---
+++ src/main/java/schafer/kyle/instagram_data/InstagramDataApplication.java
@@ -0,0 +1,13 @@
+package schafer.kyle.instagram_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/schafer/kyle/instagram_data/InstagramDataApplicationTests.java
@@ -0,0 +1,13 @@
+package schafer.kyle.instagram_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
